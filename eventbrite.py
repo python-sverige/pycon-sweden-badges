68633cb24560177e5cea821b5bea7863342fce60
Helio Loureiro <helio@loureiro.eng.br>
2019-10-15 21:58:51 +0200

    Added support to blanks.

--- eventbrite.py
+++ eventbrite.py
@@ -8,13 +8,27 @@ import subprocess
 import textwrap
 import sys
 
+BADGESIZE = "80x50"
+DEFAULTBACKGROUND = "background.png"
+BLANKS = 30
+BADGES = {
+	"Business" : "background.png",
+	"Business - Invoiced" : "background.png",
+	"Early Bird" : "background.png",
+	"Personal" : "background.png",
+	"Speakers" : "background_speaker.png",
+	"Student" : "background_student.png",
+	"Volunteers and board" : "background_organization.png"
+}
+
 class BadgePrinter:
 
 	def __init__(self):
 
 		# sizes in mm
-		self.badge_width = 85
-		self.badge_height= 54
+
+		self.badge_width = int(BADGESIZE.split("x")[0]) + 4
+		self.badge_height= int(BADGESIZE.split("x")[1]) + 4
 		self.badge_padding = 2
 
 		self.paper_width = 210 # A4 paper
@@ -66,7 +80,7 @@ class BadgePrinter:
 	def tex_newpage(self):
 		self.tex_document += self._tex_newpage
 
-	def add_badge(self, position, name, affiliation, flipside=False):
+	def add_badge(self, position, name, affiliation, background, flipside=False):
 		# left or right badge, x=0 -> left, x=1 -> right
 		x = position % 2
 		# badge number from top
@@ -83,19 +97,19 @@ class BadgePrinter:
 		self.tex_document += textwrap.dedent(r'''			\AddToShipoutPictureBG*{
 				%%\put(%smm,%smm){\framebox(%smm,%smm){}}
 				\put(%smm,%smm){\framebox(%smm,%smm){}}
-				\put(%smm,%smm){\includegraphics[width=81mm,height=50mm]{background.png}}
+				\put(%smm,%smm){\includegraphics[width=81mm,height=50mm]{%s}}
 				\put(%smm,%smm){\makebox(%smm,%smm){\parbox{80mm}{\centering{\fontsize{20}{20}\selectfont\textbf{%s}\\\vspace{2mm}\fontsize{12}{12}\selectfont\textit{%s}}}}}
 			}
 		''' % (
 			left_margin, -(self.page_topmargin + y*self.badge_height), self.badge_width, self.badge_height,
 			left_margin + self.badge_padding, -(self.page_topmargin + y*self.badge_height - self.badge_padding), badge_inner_width, badge_inner_height,
-			left_margin + self.badge_padding, -(self.page_topmargin + (y-1)*self.badge_height + self.badge_padding+50),
+			left_margin + self.badge_padding, -(self.page_topmargin + (y-1)*self.badge_height + self.badge_padding+50), background,
 			left_margin + self.badge_padding, -(self.page_topmargin + y*self.badge_height - self.badge_padding), badge_inner_width, badge_inner_height - self.header_height, name, affiliation)
 		)
 
-	def next_badge(self, name, affiliation):
+	def next_badge(self, name, affiliation, background):
 		affiliation = affiliation.replace('&', '\\&')
-		self.add_badge(self.badge_counter % 10, name, affiliation)
+		self.add_badge(self.badge_counter % 10, name, affiliation, background)
 
 		self.backside.append((name, affiliation))
 
@@ -103,9 +117,9 @@ class BadgePrinter:
 
 		if self.badge_counter % 10 == 0:
 			self.tex_newpage()
-			self.flush_backside()
+			self.flush_backside(background)
 
-	def flush_backside(self):
+	def flush_backside(self, background):
 		backsideSize = int(len(self.backside)/2)
 		for i in range(backsideSize):
 			tmp = self.backside[2*i]
@@ -118,15 +132,15 @@ class BadgePrinter:
 
 		for i, r in enumerate(self.backside):
 			if r is not None:
-				self.add_badge(i, r[0], r[1], flipside=True)
+				self.add_badge(i, r[0], r[1], background, flipside=True)
 
 		self.backside = []
 		self.tex_newpage()
 
-	def flush_badges(self):
+	def flush_badges(self, background):
 		if self.backside:
 			self.tex_newpage()
-			self.flush_backside()
+			self.flush_backside(background)
 
 def main(args):
 	with open(args.csv_file, encoding='utf-8') as f:
@@ -134,21 +148,39 @@ def main(args):
 		b = BadgePrinter()
 		b.tex_header()
 		reader = csv.DictReader(f)
+		counter = 0
 		for row in reader:
 			#if (args.limit and row['Order #'] in args.limit) or not args.limit:
 			#	b.next_badge(row['First Name'], row['Last Name'])
 
+			if row["Attendee Status"] != "Attending":
+				continue
 			fullName = row['First Name'] + " " + row['Last Name']
 			company = row['Company']
 			ticketType = row['Ticket Type']
 			jobTitle = row['Job Title']
-			#print(fullName, company, ticketType, jobTitle)
-			b.next_badge(fullName, jobTitle)
+			if len(company) > 0:
+				if len(jobTitle) > 0:
+					jobTitle += " at " + company
+				else:
+					jobTitle = company
+			print(counter, fullName, company, ticketType, jobTitle)
+			if not ticketType in BADGES:
+				background = DEFAULTBACKGROUND
+			else:
+				background = BADGES[ticketType]
+			b.next_badge(fullName, jobTitle, background)
+			counter += 1
+		for x in range(BLANKS):
+			print("Blank: ", x)
+			b.next_badge("", "", DEFAULTBACKGROUND)
 
-	b.flush_badges()
+	#b.flush_badges(DEFAULTBACKGROUND)
 	b.tex_footer()
 
-	p = subprocess.Popen(['pdflatex', '-jobname=badges'], stdin=subprocess.PIPE,
+	p = subprocess.Popen(['pdflatex', '-jobname=badges'],
+			stdin=subprocess.PIPE,
+			stdout=subprocess.DEVNULL,
 			encoding='utf8')
 
 	with open('debug.tex', 'w', encoding="utf-8") as f:
